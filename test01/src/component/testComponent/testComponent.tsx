83373765cc070a8e61c6634791368d57d45da4f2
chry8822 <goggg8822@naver.com>
2022-05-27 16:09:40 +0900

    test type

--- test01/src/component/testComponent/testComponent.tsx
+++ test01/src/component/testComponent/testComponent.tsx
@@ -1,9 +1,77 @@
-import React from 'react';
+import React,{ useState } from 'react';
 
 const TestComponent = () => {
+    const [showContent, setShowContent] = useState(-1);
+    const [inputContent, setInputContent] =useState("");
+    
+    interface User {
+        id: string;
+        name: string;
+        age: number;
+        address: string;
+    }
+
+   
+    const info = {
+        id: "goggg8822",
+        name: "chrys",
+        age: 33,
+        address: "goggg8822@naver.com"
+    }
+
+    function testFunction  (info :User)  {
+        return (
+            <ul>
+                <li>
+                    id: `${info.id}`
+                </li>
+                <li>
+                    name: `${info.name}`
+                </li>
+                <li>
+                    age: `${info.age}`
+                </li>
+                <li>
+                    address: `${info.address}`
+                </li>
+            </ul>
+        )
+    }
+
+    const toggleHandler = () => {
+        if(showContent === -1) {
+            setShowContent(1)
+        } else {
+            setShowContent(-1)
+        }
+    }
+
+    const inputContentHandler = (e: any) => {
+        const target = e.target.value;
+        setInputContent(target)
+    }
+
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setInputContent("");
+    }
+
+
     return (
         <div>
-            <button>test button</button>
+            <button onClick={toggleHandler}>test button</button>
+          {
+              showContent === 1 ? 
+              testFunction(info) :
+              ""
+          }
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="testSub"></label>
+            <input onChange={inputContentHandler} type="text" name='testSub'/>
+            <button type='submit'>submit</button>
+          </form>
+          <div>{inputContent}</div>
         </div>
     )
 }
