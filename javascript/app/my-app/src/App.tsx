f17ff2eb6a9669ac246df43d913d11d1821c5c53
chry8822 <okh8822@hmcnerworks.co.kr>
2022-08-11 11:42:06 +0900

    baseball game in react

--- javascript/app/my-app/src/App.tsx
+++ javascript/app/my-app/src/App.tsx
@@ -1,27 +1,36 @@
-import React,{ useState } from 'react';
+import React,{ useState, useCallback, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  let list:number[] = [0,1,2,3,4,5,6,7,8,9];
-  let number:any = [];
+  const [inputNumber, setInput] = useState<any>("")
+  const [number, setNumber] = useState<any>([])
 
+  let list:number[] = [0,1,2,3,4,5,6,7,8,9];
+  
+
+useEffect(() => {
+  let inNumber:any = []
   for(let i = 0; i < 4; i++){
     let select = Math.floor(Math.random() * list.length);
-    number[i] = list.splice(select, 1)[0];
-  }
+    if(number.length < 5){
+        inNumber[i] = list.splice(select, 1)[0]
+        setNumber(inNumber)
+      }
+    }
+  },[])
 
-  let count:number = 0;
   let strike:number = 0;
   let ball:number = 0;
-  while(count < 10){
-    console.log("asfasdfdas")
-    let input:any = prompt("input number") || "";
+  let testCount = 0 ;
+
+const test = () => {
+  if(testCount <= 10){
+    let input:any = inputNumber;
     let inputArray:any = input.split('');
-    // console.log(inputArray)
     strike = 0;
     ball = 0;
-    count ++;
+    testCount++
     for (let i = 0; i < 4; i++){
       for(let j = 0; j < 4; j++){
         if(number[i] == inputArray[j]){
@@ -35,25 +44,43 @@ function App() {
       }
     }
     if(strike === 4){
-      alert("HomeRun!!" + "for count" + (count - 1) )
-      break;
-    }else if(count >= 10){
-      console.error('count over')
+      console.log("HomeRun!!" + "for count" + (testCount - 1) )
+    }else if(testCount >= 10){
+      console.log('count over')
     }else {
-      alert(inputArray.join('') + ': ' + strike + ": strike " + ball + ": ball ")
+      console.log(inputArray.join('') + ': ' + strike + ": strike " + ball + ": ball ")
     }
   }
+}
+    
 
+const submitNum = useCallback((e:any) => {
+  e.preventDefault()
+  if(inputNumber.length !== 0){
+    test()
+  }
+},[inputNumber])
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-          <input type="text"
-            onChange={()=>{
-
-            }}
-          />
+        <h2>attact Number</h2>
+         <form>
+            <label htmlFor="attact"></label>
+            <input 
+                type="text" 
+                id='attact' 
+                name='attact'
+                autoComplete="off"
+                value={inputNumber}
+                maxLength={4}
+                onChange={(e)=>{
+                setInput(e.target.value)
+              }}
+            />
+            <button type='submit' onClick={(e) => submitNum(e)}>enter</button>
+         </form>
       </header>
     </div>
   );
