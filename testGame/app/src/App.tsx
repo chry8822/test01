0ce6b8af5b6d4a80e8389d4c27cefbc814260588
chry8822 <okh8822@hmcnerworks.co.kr>
2022-08-19 16:49:00 +0900

    test game

---
+++ testGame/app/src/App.tsx
@@ -0,0 +1,131 @@
+import React, { useState, useMemo } from 'react';
+import { dummy } from './testDummy'
+
+function App() {
+  const [select, setSelect] = useState<any>(
+    {
+      player: 0,
+      genre: 0,
+      level: 0
+    }
+  )
+
+
+  const renderGame = useMemo(() => {
+    let data: any[] = [];
+    dummy.forEach((item: any, idx: any) => {
+      if (select.player <= item.player && select.genre === 0 && select.level === 0) { // 인원
+        data.push(
+          <li key={idx}>{item.title}</li>
+        )
+        return
+      } else if (select.genre === item.genre && select.player === 0 && select.level === 0) { // 장르
+        data.push(
+          <li key={idx}>{item.title}</li>
+        )
+        return
+      } else if (select.level === item.level && select.player === 0 && select.genre === 0) { // 난이도
+        data.push(
+          <li key={idx}>{item.title}</li>
+        )
+        return
+      } else if (select.player <= item.player && select.genre === item.genre && select.level === 0){ // 인원 && 장르
+        data.push(
+          <li key={idx}>{item.title}</li>
+        )
+        return
+      } else if (select.level === item.level && select.genre === item.genre && select.player === 0){ // 레벨 && 장르
+        data.push(
+          <li key={idx}>{item.title}</li>
+        )
+        return
+      } else if (select.player <= item.player && select.level === item.level && select.genre === item.genre){ // 레벨 && 인원
+        data.push(
+          <li key={idx}>{item.title}</li>
+        )
+        return
+      } 
+      else if (select.level === item.level && select.genre === item.genre && select.player <= item.player){ //레벨 && 장르 && 인원
+        data.push(
+          <li key={idx}>{item.title}</li>
+        )
+        return
+      }
+
+
+      else if (
+        select.player === 0 && select.genre === 0 && select.level === 0
+      ) {
+        data.push(
+          <li key={idx}>{item.title}</li>
+        )
+        return
+      }
+    })
+    return data
+  }, [select.player, select.genre, select.level])
+
+
+  const test = useMemo(() => {
+    let data:any ;
+    data = dummy.filter((item:any, idx:any) => {
+      if(select.player >= item.player){
+        return item
+      }else if(select.genre === item.genre){
+        return item
+      }else if(select.level === item.level){
+        return item
+      }else if(select.player === 0 && select.genre === 0 && select.level ===0){
+        return
+      }
+    })
+
+    return (
+      data.map((item:any, idx:any) => {
+        return (
+          <li key={idx}>{item.title}</li>
+        )
+      })
+    )
+  },[select.player, select.genre, select.level])
+
+
+
+  return (
+    <div className="App">
+      <div>
+        <ul>
+          {/* {renderGame} */}
+          {test}
+        </ul>
+      </div>
+      <form action="">
+        <select onChange={(e) => setSelect({ ...select, player: Number(e.target.value) })}>
+          <option value="0">player</option>
+          <option value="1">1인</option>
+          <option value="2">2인</option>
+          <option value="3">3인</option>
+          <option value="4">4인</option>
+        </select>
+        <select onChange={(e) => setSelect({ ...select, genre: Number(e.target.value) })}>
+          <option value="0">genre</option>
+          <option value="1">카드</option>
+          <option value="2">탈출</option>
+          <option value="3">모험</option>
+          <option value="4">추리</option>
+          <option value="5">공부</option>
+        </select>
+        <select onChange={(e) => setSelect({ ...select, level: Number(e.target.value) })}>
+          <option value="0">level</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+        </select>
+      </form>
+    </div>
+  );
+}
+
+export default App;
---
+++ testGame/app/src/testDummy.ts
@@ -0,0 +1,110 @@
+export const dummy = [
+{
+    title: "마피아",
+    player: 4,
+    genre: 3,
+    level:5  
+},
+{
+    title: "갈리",
+    player: 2,
+    genre: 1,
+    level:3  
+},
+{
+    title: "뱅",
+    player: 1,
+    genre: 1,
+    level:5  
+},
+{
+    title: "마리오",
+    player: 4,
+    genre: 3,
+    level:2  
+},
+{
+    title: "원정대",
+    player: 4,
+    genre: 3,
+    level:4  
+},
+{
+    title: "지저스",
+    player: 2,
+    genre: 3,
+    level:5  
+},
+{
+    title: "부르마블",
+    player: 2,
+    genre: 3,
+    level:3  
+},
+{
+    title: "응급실",
+    player: 2,
+    genre: 6,
+    level:1  
+},
+{
+    title: "리액트",
+    player: 3,
+    genre: 2,
+    level:3  
+},
+{
+    title: "스크립트",
+    player: 1,
+    genre: 2,
+    level:2  
+},
+{
+    title: "배그",
+    player: 1,
+    genre: 6,
+    level:5  
+},
+{
+    title: "레이싱",
+    player: 2,
+    genre: 6,
+    level:5  
+},
+{
+    title: "라디오",
+    player: 2,
+    genre: 5,
+    level:2  
+},
+{
+    title: "땅콩",
+    player: 2,
+    genre: 4,
+    level:3  
+},
+{
+    title: "애플",
+    player: 2,
+    genre: 4,
+    level:3  
+},
+{
+    title: "구글",
+    player: 1,
+    genre: 5,
+    level:1  
+},
+{
+    title: "원정대2",
+    player: 4,
+    genre: 3,
+    level:4  
+},
+{
+    title: "원정대3",
+    player: 4,
+    genre: 3,
+    level:4  
+},
+]
