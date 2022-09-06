f6374bd32af44a2c3040f21497589f7de809bec5
chry8822 <okh8822@hmcnerworks.co.kr>
2022-09-06 10:45:45 +0900

    test timer

--- javascript/test-recursive/app/src/App.test.tsx
+++ javascript/test-recursive/app/src/App.test.tsx
@@ -7,3 +7,60 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+
+
+
+
+// const timerStart = useCallback((e: any) => {
+//   setValueFlag(false)
+//   e.preventDefault()
+//   if (intervalTimer.current) {
+//     return
+//   }
+//   intervalTimer.current = setInterval(() => {
+//     setTimerValue((time: any) => {
+//       if (time.sec > 0) {
+//         console.log("sec")
+//         setValueFlag(false)
+//         return {
+//           ...time,
+//           sec: time.sec - 1
+//         }
+//       } else if (time.sec === 0 || time.sec === "") {
+//         if (time.min === 0 || time.min === "") {
+//           if (time.hour === 0 || time.hour === "") {
+//             setValueFlag(true)
+//             setTimerShowFlag(false)
+//             clearInterval(intervalTimer.current);
+//             intervalTimer.current = null
+//             return {
+//               ...time,
+//               hour: "",
+//               min: "",
+//               sec: "",
+//             }
+//           }
+//         } else if (time.min > 0) {
+//           console.log("min")
+//           setValueFlag(false)
+//           return {
+//             ...time,
+//             min: time.min - 1,
+//             sec: 59
+//           }
+//         } if (time.hour > 0) {
+//           console.log("hour")
+//           setValueFlag(false)
+//           return {
+//             ...time,
+//             hour: time.hour - 1,
+//             min: 60,
+//             sec: 0
+//           }
+//         }
+//       }
+//     })
+
+//   }, 1000)
+// }, [{ ...timerValue }])
--- javascript/test-recursive/app/src/App.tsx
+++ javascript/test-recursive/app/src/App.tsx
@@ -47,58 +47,53 @@ function App() {
 
 
   const timerStart = useCallback((e: any) => {
-    setValueFlag(false)
-    e.preventDefault()
-    if (intervalTimer.current) {
-      return
-    }
-    intervalTimer.current = setInterval(() => {
-      setTimerValue((time: any) => {
-        if (time.sec > 0) {
-          console.log("sec")
-          setValueFlag(false)
-          return {
-            ...time,
-            sec: time.sec - 1
-          }
-        } else if (time.sec === 0 || time.sec === "") {
-          if (time.min === 0 || time.min === "") {
-            if (time.hour === 0 || time.hour === "") {
-              setValueFlag(true)
-              setTimerShowFlag(false)
-              clearInterval(intervalTimer.current);
-              intervalTimer.current = null
+      setValueFlag(false);
+      e.preventDefault();
+      if (intervalTimer.current) {
+        return;
+      }
+      intervalTimer.current = setInterval(() => {
+        setTimerValue((time: any) => {
+          if (time.sec > 0) {
+            setValueFlag(false);
+            return {
+              ...time,
+              sec: time.sec - 1,
+            };
+          } else if (time.sec === 0 || time.sec === '') {
+            if (time.min === 0 || time.min === '') {
+              if (time.hour === 0 || time.hour === '') {
+                setValueFlag(true);
+                setTimerShowFlag(false);
+                clearInterval(intervalTimer.current);
+                intervalTimer.current = null;
+                return {
+                  ...time,
+                  hour: '',
+                  min: '',
+                  sec: '',
+                };
+              } else if (time.hour > 0) {
+                setValueFlag(false);
+                return {
+                  ...time,
+                  hour: time.hour - 1,
+                  min: 60,
+                  sec: 0,
+                };
+              }
+            } else if (time.min > 0) {
+              setValueFlag(false);
               return {
                 ...time,
-                hour: "",
-                min: "",
-                sec: "",
-              }
-            }
-          } else if (time.min > 0) {
-            console.log("min")
-            setValueFlag(false)
-            return {
-              ...time,
-              min: time.min - 1,
-              sec: 59
-            }
-          } if (time.hour > 0) {
-            console.log("hour")
-            setValueFlag(false)
-            return {
-              ...time,
-              hour: time.hour - 1,
-              min: 60,
-              sec: 0
+                min: time.min - 1,
+                sec: 59,
+              };
             }
           }
-        }
-      })
-
-    }, 1000)
-  }, [{ ...timerValue }])
-
+        });
+      }, 1000);
+    },[{ ...timerValue }]);
 
 
   const resetValue = useCallback(() => {
